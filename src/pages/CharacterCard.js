e3f81bf85f2bbc717b2534e39b13acc8473d8256
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-10-03 22:46:48 +0200

    API integrated

---
+++ src/pages/CharacterCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CharacterCard = (props) => {
+console.log(props)
+ return (
+    <div className="block p-1 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.characters.name}</h5>
+    <img src={props.characters.image}/>
+    </div>
+  )
+}
+
+export default CharacterCard
