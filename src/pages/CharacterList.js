dd7b3203d05d688fe27a9b176b07e35bff7b8f39
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-10-07 23:33:08 +0200

    Intermediate Commit

--- src/pages/CharacterList.js
+++ src/pages/CharacterList.js
@@ -21,7 +21,7 @@ if(error) return <div>Errrorrrr....</div>
       {/* <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div> */}
-        <input value={search} onChange={(e)=> searchItems(e.target.value)} type="search" id="default-search" className="block mb-10 p-4 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required></input>
+        <input value={search} onChange={(e)=> searchItems(e.target.value)} type="search" id="default-search" className="block mb-10 p-4 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search For Characters.." required></input>
     <div className="flex flex-wrap space-x-4">
      
       {data.characters.results.map((character) =>{
