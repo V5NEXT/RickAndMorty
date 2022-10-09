3b1ee92d57de161f1c5f829fdbc01c08e1c3a089
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-10-09 13:57:52 +0200

    Added Other Components

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Home from './component/Home';
 import CharacterList from './pages/CharacterList';
 import Episodes from './pages/Episodes';
 import Locations from './pages/Locations';
+import CharacterDetails from './pages/CharacterDetails';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path='/' exact element={Home}/>
-        <Route path='/characters' element={CharacterList}/>
-        <Route path='/episodes' element={Episodes}/>
-        <Route path='/locations' element={Locations}/>
+        <Route path='/' exact element={<Home/>}/>
+        <Route path='/characters' element={<CharacterList/>}/>
+        <Route path='/character/:id' element={<CharacterDetails/>}/>
+        <Route path='/episodes' element={<Episodes/>}/>
+        <Route path='/locations' element={<Locations/>}/>
       </Routes>
     </BrowserRouter>
 
