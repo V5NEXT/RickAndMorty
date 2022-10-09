d66740b9e3b759bcb63b75dd83725e74adc36382
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-10-09 13:15:21 +0200

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import CharacterList from './pages/CharacterList';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
+      <Navbar/>
       <CharacterList/>
     </div>
+    </BrowserRouter>
+
   );
 }
 
