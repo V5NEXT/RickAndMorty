ae3b7f9d3d0578d281d9d69330deec3ca775aacc
Vishnu Viswambharan <vishnuviswambharan1994@gmail.com>
2022-10-09 13:27:01 +0200

    Added Pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import CharacterList from './pages/CharacterList';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Navbar from './component/Navbar';
+import Home from './component/Home';
+import CharacterList from './pages/CharacterList';
+import Episodes from './pages/Episodes';
+import Locations from './pages/Locations';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
       <Navbar/>
-      <CharacterList/>
-    </div>
+      <Routes>
+        <Route path='/' exact element={Home}/>
+        <Route path='/characters' element={CharacterList}/>
+        <Route path='/episodes' element={Episodes}/>
+        <Route path='/locations' element={Locations}/>
+      </Routes>
     </BrowserRouter>
 
   );
